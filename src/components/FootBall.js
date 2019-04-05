90f43b9cc77c01ffa8dc30791009025cc873b82b
Pasha Hliebov <pallke2015@gmail.com>
2019-04-05 22:42:21 +0300

    Рекат Роутер добавил

--- src/components/FootBall.js
+++ src/components/FootBall.js
@@ -1,30 +1,46 @@
 import React , { Component }from 'react'
 import { connect } from 'react-redux';
+import {  Switch, Route } from "react-router-dom";
 
 
 import League from './league'
 import Modal from './modal'
 import Table from './table'
+import Header from "./header";
 
 
 class FootBall extends Component {
 
-    render() {
+
+  render() {
       const { showLastsMatches } = this.props;
       const modal = showLastsMatches ? <div className="custom-modal-wrapper">
         <Modal />
       </div> : null;
       return (
         <div>
-          <ul className="list-group">
-            <League/>
-            <li className="list-group-item">Premier League</li>
-          </ul>
-          { modal }
+        <Header/>
 
-        <Table />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={()=>{
+            return(
+              <div>
+                <ul className="list-group">
+                  <League/>
+                  <li className="list-group-item">Premier League</li>
+                </ul>
+                { modal }
+              </div>
+            )
+          }}/>
+
+          <Route path="/table" component={Table} exact />
+        </Switch>
+
         </div>
-
       )
     }
 }
@@ -33,4 +49,4 @@ const mapStateToProps = (state) => ({
 });
 
 
-export default connect(mapStateToProps)(FootBall);
+export default connect(mapStateToProps,)(FootBall);
