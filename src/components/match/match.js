0aa8d78d88d5c62e41b4bbac96a389ca15946413
Pasha Hliebov <pallke2015@gmail.com>
2019-05-07 13:21:40 +0300

    name files fixed

--- src/components/match/match.js
+++ src/components/match/match.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { MODAL } from "../../actionCreators/team-Info/modal";
+import { MODAL } from "../../actionCreators/team-info/modal";
 
 
 class Match extends Component {
